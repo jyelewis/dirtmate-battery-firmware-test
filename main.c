537e37e53c4c56f5367a7bac6195ebbfe497d878
Joshua Lloyd <jlloyd@Joshuas-MacBook-Air.local>
2023-03-02 16:18:47 +1100

    Testing for switching mosfets in battery pack

--- main.c
+++ main.c
@@ -18,27 +18,27 @@ void run_test(char* test_name, SystemInputState input_state, SystemOutputState e
     }
 
     // test failed :(
-    printf("Test: %s FAILED\n", test_name);
+    printf("\tTest: %s FAILED\n", test_name);
     if (expected_output_state.top_output_enable != actual_output_state.top_output_enable) {
         print_pin_state("\tExpected top_output_enable to be", expected_output_state.top_output_enable);
         print_pin_state("\tBut it was", actual_output_state.top_output_enable);
     }
 
-    printf("Test: %s FAILED\n", test_name);
+    printf("\tTest: %s FAILED\n", test_name);
     if (expected_output_state.bottom_output_enable != actual_output_state.bottom_output_enable) {
-        print_pin_state("\tExpected top_output_enable to be", expected_output_state.bottom_output_enable);
+        print_pin_state("\tExpected bottom_output_enable to be", expected_output_state.bottom_output_enable);
         print_pin_state("\tBut it was", actual_output_state.bottom_output_enable);
     }
 
-    printf("Test: %s FAILED\n", test_name);
+    printf("\tTest: %s FAILED\n", test_name);
     if (expected_output_state.port1_output_enable != actual_output_state.port1_output_enable) {
-        print_pin_state("\tExpected top_output_enable to be", expected_output_state.port1_output_enable);
+        print_pin_state("\tExpected port1_output_enable to be", expected_output_state.port1_output_enable);
         print_pin_state("\tBut it was", actual_output_state.port1_output_enable);
     }
 
-    printf("Test: %s FAILED\n", test_name);
+    printf("\tTest: %s FAILED\n", test_name);
     if (expected_output_state.port2_output_enable != actual_output_state.port2_output_enable) {
-        print_pin_state("\tExpected top_output_enable to be", expected_output_state.port2_output_enable);
+        print_pin_state("\tExpected port2_output_enable to be", expected_output_state.port2_output_enable);
         print_pin_state("\tBut it was", actual_output_state.port2_output_enable);
     }
 }
@@ -88,6 +88,406 @@ int main() {
         },
     (SystemOutputState){
                 .top_output_enable = HIGH,
+                .bottom_output_enable = LOW,
+                .port1_output_enable = LOW,
+                .port2_output_enable = LOW,
+        }
+    );
+
+run_test(
+            "Puck connected via port 1, no input sources, battery is full",
+            (SystemInputState){
+                .internal_battery_voltage = 1.0,
+
+                .top_state_bool_low = 0.0,
+                .top_state_bool_high = 0.0,
+
+                .bottom_state_bool_low = 0.0,
+                .bottom_state_bool_high = 0.0,
+
+                .port1_state_bool_low = 1.0,
+                .port1_state_bool_high = 0.0,
+
+                .port2_state_bool_low = 0.0,
+                .port2_state_bool_high = 0.0,
+        },
+    (SystemOutputState){
+                .top_output_enable = LOW,
+                .bottom_output_enable = LOW,
+                .port1_output_enable = HIGH,
+                .port2_output_enable = LOW,
+        }
+    );
+
+    run_test(
+            "Puck connected via port 2, no input sources, battery is full",
+            (SystemInputState){
+                .internal_battery_voltage = 1.0,
+
+                .top_state_bool_low = 0.0,
+                .top_state_bool_high = 0.0,
+
+                .bottom_state_bool_low = 0.0,
+                .bottom_state_bool_high = 0.0,
+
+                .port1_state_bool_low = 0.0,
+                .port1_state_bool_high = 0.0,
+
+                .port2_state_bool_low = 1.0,
+                .port2_state_bool_high = 0.0,
+        },
+    (SystemOutputState){
+                .top_output_enable = LOW,
+                .bottom_output_enable = LOW,
+                .port1_output_enable = LOW,
+                .port2_output_enable = HIGH,
+        }
+    );
+
+        run_test(
+            "Puck connected via top pins, no input sources, battery is 50%",
+            (SystemInputState){
+                .internal_battery_voltage = 0.5,
+
+                .top_state_bool_low = 1.0,
+                .top_state_bool_high = 0.0,
+
+                .bottom_state_bool_low = 0.0,
+                .bottom_state_bool_high = 0.0,
+
+                .port1_state_bool_low = 0.0,
+                .port1_state_bool_high = 0.0,
+
+                .port2_state_bool_low = 0.0,
+                .port2_state_bool_high = 0.0,
+        },
+    (SystemOutputState){
+                .top_output_enable = HIGH,
+                .bottom_output_enable = LOW,
+                .port1_output_enable = LOW,
+                .port2_output_enable = LOW,
+        }
+    );
+
+            run_test(
+            "Puck Not Connected, Battery Connected via Bottom Pins, battery is full",
+            (SystemInputState){
+                .internal_battery_voltage = 1.0,
+
+                .top_state_bool_low = 0.0,
+                .top_state_bool_high = 0.0,
+
+                .bottom_state_bool_low = 1.0,
+                .bottom_state_bool_high = 1.0,
+
+                .port1_state_bool_low = 0.0,
+                .port1_state_bool_high = 0.0,
+
+                .port2_state_bool_low = 0.0,
+                .port2_state_bool_high = 0.0,
+        },
+    (SystemOutputState){
+                .top_output_enable = LOW,
+                .bottom_output_enable = HIGH,
+                .port1_output_enable = LOW,
+                .port2_output_enable = LOW,
+        }
+    );
+
+ run_test(
+            "Puck Not Connected, Battery Connected via Bottom Pins, battery is at 60%",
+            (SystemInputState){
+                .internal_battery_voltage = 0.6,
+
+                .top_state_bool_low = 0.0,
+                .top_state_bool_high = 0.0,
+
+                .bottom_state_bool_low = 1.0,
+                .bottom_state_bool_high = 1.0,
+
+                .port1_state_bool_low = 0.0,
+                .port1_state_bool_high = 0.0,
+
+                .port2_state_bool_low = 0.0,
+                .port2_state_bool_high = 0.0,
+        },
+    (SystemOutputState){
+                .top_output_enable = LOW,
+                .bottom_output_enable = LOW,
+                .port1_output_enable = LOW,
+                .port2_output_enable = LOW,
+        }
+    );
+
+    run_test(
+            "Puck Connected, Battery Connected via Bottom Pins, battery is at 40%",
+            (SystemInputState){
+                .internal_battery_voltage = 0.4,
+
+                .top_state_bool_low = 1.0,
+                .top_state_bool_high = 0.0,
+
+                .bottom_state_bool_low = 1.0,
+                .bottom_state_bool_high = 1.0,
+
+                .port1_state_bool_low = 0.0,
+                .port1_state_bool_high = 0.0,
+
+                .port2_state_bool_low = 0.0,
+                .port2_state_bool_high = 0.0,
+        },
+    (SystemOutputState){
+                .top_output_enable = HIGH,
+                .bottom_output_enable = LOW,
+                .port1_output_enable = LOW,
+                .port2_output_enable = LOW,
+        }
+    );
+
+    run_test(
+            "Puck Connected, Aux Device in Port 1, battery is full",
+            (SystemInputState){
+                .internal_battery_voltage = 1.0,
+
+                .top_state_bool_low = 1.0,
+                .top_state_bool_high = 0.0,
+
+                .bottom_state_bool_low = 0.0,
+                .bottom_state_bool_high = 0.0,
+
+                .port1_state_bool_low = 0.0,
+                .port1_state_bool_high = 1.0,
+
+                .port2_state_bool_low = 0.0,
+                .port2_state_bool_high = 0.0,
+        },
+    (SystemOutputState){
+                .top_output_enable = HIGH,
+                .bottom_output_enable = LOW,
+                .port1_output_enable = HIGH,
+                .port2_output_enable = LOW,
+        }
+    );
+
+run_test(
+            "Puck Connected, Aux Device in Port 1, battery is 40%",
+            (SystemInputState){
+                .internal_battery_voltage = 0.4,
+
+                .top_state_bool_low = 1.0,
+                .top_state_bool_high = 0.0,
+
+                .bottom_state_bool_low = 0.0,
+                .bottom_state_bool_high = 0.0,
+
+                .port1_state_bool_low = 0.0,
+                .port1_state_bool_high = 1.0,
+
+                .port2_state_bool_low = 0.0,
+                .port2_state_bool_high = 0.0,
+        },
+    (SystemOutputState){
+                .top_output_enable = HIGH,
+                .bottom_output_enable = LOW,
+                .port1_output_enable = LOW,
+                .port2_output_enable = LOW,
+        }
+    );
+
+run_test(
+            "Puck connected on top, second battery bottom pins, ext obj port 1, second puck port 2, battery is full",
+            (SystemInputState){
+                .internal_battery_voltage = 1.0,
+
+                .top_state_bool_low = 1.0,
+                .top_state_bool_high = 0.0,
+
+                .bottom_state_bool_low = 1.0,
+                .bottom_state_bool_high = 1.0,
+
+                .port1_state_bool_low = 0.0,
+                .port1_state_bool_high = 1.0,
+
+                .port2_state_bool_low = 1.0,
+                .port2_state_bool_high = 0.0,
+        },
+    (SystemOutputState){
+                .top_output_enable = HIGH,
+                .bottom_output_enable = HIGH,
+                .port1_output_enable = HIGH,
+                .port2_output_enable = HIGH,
+        }
+    );
+
+    run_test(
+            "Puck connected on top, second battery bottom pins, ext obj port 1, second puck port 2, battery is 70%",
+            (SystemInputState){
+                .internal_battery_voltage = 0.7,
+
+                .top_state_bool_low = 1.0,
+                .top_state_bool_high = 0.0,
+
+                .bottom_state_bool_low = 1.0,
+                .bottom_state_bool_high = 1.0,
+
+                .port1_state_bool_low = 0.0,
+                .port1_state_bool_high = 1.0,
+
+                .port2_state_bool_low = 1.0,
+                .port2_state_bool_high = 0.0,
+        },
+    (SystemOutputState){
+                .top_output_enable = HIGH,
+                .bottom_output_enable = LOW,
+                .port1_output_enable = HIGH,
+                .port2_output_enable = HIGH,
+        }
+    );
+
+     run_test(
+            "Puck connected on top, second battery bottom pins, ext obj port 1, second puck port 2, battery is 40%",
+            (SystemInputState){
+                .internal_battery_voltage = 0.4,
+
+                .top_state_bool_low = 1.0,
+                .top_state_bool_high = 0.0,
+
+                .bottom_state_bool_low = 1.0,
+                .bottom_state_bool_high = 1.0,
+
+                .port1_state_bool_low = 0.0,
+                .port1_state_bool_high = 1.0,
+
+                .port2_state_bool_low = 1.0,
+                .port2_state_bool_high = 0.0,
+        },
+    (SystemOutputState){
+                .top_output_enable = HIGH,
+                .bottom_output_enable = LOW,
+                .port1_output_enable = LOW,
+                .port2_output_enable = HIGH,
+        }
+    );
+    
+        run_test(
+            "Puck connected on top, second battery bottom pins, ext obj port 1, second puck port 2, battery is 10%",
+            (SystemInputState){
+                .internal_battery_voltage = 0.1,
+
+                .top_state_bool_low = 1.0,
+                .top_state_bool_high = 0.0,
+
+                .bottom_state_bool_low = 1.0,
+                .bottom_state_bool_high = 1.0,
+
+                .port1_state_bool_low = 0.0,
+                .port1_state_bool_high = 1.0,
+
+                .port2_state_bool_low = 1.0,
+                .port2_state_bool_high = 0.0,
+        },
+    (SystemOutputState){
+                .top_output_enable = LOW,
+                .bottom_output_enable = LOW,
+                .port1_output_enable = LOW,
+                .port2_output_enable = LOW,
+        }
+    );
+
+        run_test(
+            "Battery connected on top, second battery bottom pins, ext obj port 1, puck port 2, battery is full",
+            (SystemInputState){
+                .internal_battery_voltage = 1.0,
+
+                .top_state_bool_low = 1.0,
+                .top_state_bool_high = 1.0,
+
+                .bottom_state_bool_low = 1.0,
+                .bottom_state_bool_high = 1.0,
+
+                .port1_state_bool_low = 0.0,
+                .port1_state_bool_high = 1.0,
+
+                .port2_state_bool_low = 1.0,
+                .port2_state_bool_high = 0.0,
+        },
+    (SystemOutputState){
+                .top_output_enable = HIGH,
+                .bottom_output_enable = LOW,
+                .port1_output_enable = HIGH,
+                .port2_output_enable = HIGH,
+        }
+    );
+
+        run_test(
+            "Full Battery connected on top, second battery bottom pins, ext obj port 1, puck port 2, battery is 70%",
+            (SystemInputState){
+                .internal_battery_voltage = 0.7,
+
+                .top_state_bool_low = 0.0,
+                .top_state_bool_high = 0.0,
+
+                .bottom_state_bool_low = 1.0,
+                .bottom_state_bool_high = 1.0,
+
+                .port1_state_bool_low = 0.0,
+                .port1_state_bool_high = 1.0,
+
+                .port2_state_bool_low = 1.0,
+                .port2_state_bool_high = 0.0,
+        },
+    (SystemOutputState){
+                .top_output_enable = LOW,
+                .bottom_output_enable = LOW,
+                .port1_output_enable = HIGH,
+                .port2_output_enable = HIGH,
+        }
+    );
+
+        run_test(
+            "Battery connected on top, second battery bottom pins, ext obj port 1, puck port 2, battery is 40%",
+            (SystemInputState){
+                .internal_battery_voltage = 0.4,
+
+                .top_state_bool_low = 1.0,
+                .top_state_bool_high = 1.0,
+
+                .bottom_state_bool_low = 1.0,
+                .bottom_state_bool_high = 1.0,
+
+                .port1_state_bool_low = 0.0,
+                .port1_state_bool_high = 1.0,
+
+                .port2_state_bool_low = 1.0,
+                .port2_state_bool_high = 0.0,
+        },
+    (SystemOutputState){
+                .top_output_enable = LOW,
+                .bottom_output_enable = LOW,
+                .port1_output_enable = LOW,
+                .port2_output_enable = HIGH,
+        }
+    );
+
+            run_test(
+            "Battery connected on top, second battery bottom pins, ext obj port 1, puck port 2, battery is 10%",
+            (SystemInputState){
+                .internal_battery_voltage = 0.1,
+
+                .top_state_bool_low = 1.0,
+                .top_state_bool_high = 1.0,
+
+                .bottom_state_bool_low = 1.0,
+                .bottom_state_bool_high = 1.0,
+
+                .port1_state_bool_low = 0.0,
+                .port1_state_bool_high = 1.0,
+
+                .port2_state_bool_low = 1.0,
+                .port2_state_bool_high = 0.0,
+        },
+    (SystemOutputState){
+                .top_output_enable = LOW,
                 .bottom_output_enable = LOW,
                 .port1_output_enable = LOW,
                 .port2_output_enable = LOW,
--- update_system_state.c
+++ update_system_state.c
@@ -3,8 +3,14 @@
 //
 
 #include <printf.h>
+#include <stdbool.h>
 #include "update_system_state.h"
 #include "fake_io.h"
+
+#define LOW_BATTERY_PERCENTAGE 0.2
+#define HALF_BATTERY_PERCENTAGE 0.5
+#define HIGH_BATTERY_PERCENTAGE 0.8
+#define FULL_BATTERY_PERCENTAGE 1 
 
 typedef enum {
     // 0: no device / input power
@@ -32,19 +38,56 @@ CONNECTED_DEVICE_STATE read_connected_device_state(float high_bit, float low_bit
 void update_system_state(SystemInputState* input_state, SystemOutputState* output_state) {
     // TODO: implement me
     CONNECTED_DEVICE_STATE top_port_state = read_connected_device_state(input_state->top_state_bool_high, input_state->top_state_bool_low);
-    if (top_port_state == PORT_STATE_PUCK) {
-        // puck is connected to top port
+    CONNECTED_DEVICE_STATE bottom_port_state = read_connected_device_state(input_state->bottom_state_bool_high, input_state->bottom_state_bool_low);
+    CONNECTED_DEVICE_STATE port1_state = read_connected_device_state(input_state->port1_state_bool_high, input_state->port1_state_bool_low);
+    CONNECTED_DEVICE_STATE port2_state = read_connected_device_state(input_state->port2_state_bool_high, input_state->port2_state_bool_low);
+    double battery_percentage = input_state -> internal_battery_voltage;
+
+    output_state->top_output_enable = LOW;
+    output_state->bottom_output_enable = LOW;
+    output_state->port1_output_enable = LOW;
+    output_state->port2_output_enable = LOW;
+
+    int is_charging_external_battery = false;
+
+//Top Pins - Battery, Puck or Off
+    if (top_port_state == PORT_STATE_PUCK && battery_percentage >= LOW_BATTERY_PERCENTAGE) {
         output_state->top_output_enable = HIGH;
-        output_state->bottom_output_enable = LOW;
-        output_state->port1_output_enable = LOW;
-        output_state->port2_output_enable = LOW;
-    } else {
-        // nothing connected
-        output_state->top_output_enable = LOW;
-        output_state->bottom_output_enable = LOW;
-        output_state->port1_output_enable = LOW;
-        output_state->port2_output_enable = LOW;
+     }else if (top_port_state == PORT_STATE_EXTERNAL_BATTERY_REQUESTING_POWER && battery_percentage >= HIGH_BATTERY_PERCENTAGE && is_charging_external_battery == false){
+        output_state->top_output_enable = HIGH;
+        is_charging_external_battery = true;
+        //maybe combine these two into one
     }
+//Bottom Pins - Battery, Off, Managing Two Batteries
+    if(bottom_port_state == PORT_STATE_EXTERNAL_BATTERY_REQUESTING_POWER && battery_percentage >= HIGH_BATTERY_PERCENTAGE && is_charging_external_battery == false) {
+        output_state->bottom_output_enable = HIGH;
+        is_charging_external_battery = true;
+    } 
+
+    // Port 1 - Puck or Auxiliary Power
+    if (port1_state == PORT_STATE_PUCK && battery_percentage > LOW_BATTERY_PERCENTAGE) {
+        output_state->port1_output_enable = HIGH;
+    }else if (port1_state == PORT_STATE_AUX && battery_percentage >= HALF_BATTERY_PERCENTAGE) {
+        output_state->port1_output_enable = HIGH;
+    }
+    // Port 2 - Puck or Auxiliary Power
+    if(port2_state == PORT_STATE_AUX && battery_percentage >= HALF_BATTERY_PERCENTAGE) {
+        output_state->port2_output_enable = HIGH;
+        // Is it worth adding another statement turning both on at the same time?
+    }else if (port2_state == PORT_STATE_PUCK && battery_percentage >= LOW_BATTERY_PERCENTAGE) {
+        output_state->port2_output_enable = HIGH;
+    } 
+
+        if (battery_percentage <= LOW_BATTERY_PERCENTAGE ) {
+        return;
+        // Keeps all ports off
+    } 
+} 
+    
+  
+    
+    
 
 
-}
+
+
